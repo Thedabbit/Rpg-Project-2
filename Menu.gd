425a8c65cf6a77942abbcd886caa05fd82437b8b
PutthegundownG <filexstudios7@gmail.com>
2024-04-22 12:20:51 -0700

    Added
    I added a bunch of walls leading into the next room from starting room, fixed main menu, added character select.

--- Menu.gd
+++ Menu.gd
@@ -21,3 +21,7 @@ func _on_start_pressed():
 
 func _on_button_pressed():
 	get_tree().quit() # Replace with function body.
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://character_select_menu.tscn") # Replace with function body.

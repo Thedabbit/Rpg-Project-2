b77993c8117f9f3fc9ebfb58c2021d4914c2b0aa
PutthegundownG <filexstudios7@gmail.com>
2024-04-28 11:05:10 -0700

    e

--- CharacterBody2D2.gd
+++ CharacterBody2D2.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 var bullet_scene = preload("res://Bullet.tscn")
 var speed = 1000
 var can_shoot = true
-var bullet_shot = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,12 +9,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ($Timer.get_time_left() > 0):
-		can_shoot = false
-	else:
-		can_shoot = true
-	if Input.is_action_just_pressed("Primary Action"):
-		bullet_shot = true
 	var direction = Input.get_vector("Left2", "Right2", "Up2", "Down2")
 	velocity = direction * speed
 	move_and_slide()
@@ -25,7 +18,10 @@ func _process(delta):
 	if Input.is_action_pressed("RotateR2"):
 		rotation_degrees += 5
 	if Input.is_action_just_pressed("Primary Action") and can_shoot == true:
+		can_shoot = false
+		$Timer.start()
 		shoot_bullet()
+		
 	
 	
 func shoot_bullet():
@@ -38,6 +34,5 @@ func shoot_bullet():
 
 func _on_timer_timeout():
 	can_shoot = true
-	await get_tree().create_timer(5.0).timeout
-	bullet_shot = false
+
  # Replace with function body.

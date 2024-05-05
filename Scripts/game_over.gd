412587ab02a85e516487fe86d041ef77f20a48ec
PutthegundownG <167708770+PutthegundownG@users.noreply.github.com>
2024-05-04 23:15:16 -0700

    Fixed a bug

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -2,9 +2,11 @@ extends Control
 
 
 func _on_button_pressed():
+	Global.health = 100
 	get_tree().change_scene_to_file("res://scenes/Menu.tscn") # Replace with function body.
 
 
 func _on_quit_pressed():
 	Global.health = 100
 	get_tree().quit() # Replace with function body.
+		

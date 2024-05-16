34e6cf04b3c1c8defc71a8e4415aa5a82fc784c8
PutthegundownG <167708770+PutthegundownG@users.noreply.github.com>
2024-05-15 20:18:01 -0700

    qwe

---
+++ scenes/health_bar.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+<<<<<<< HEAD:health_bar.gd
+	$MarginContainer/TextureProgressBar.value = Global.health
+=======
+	pass
+>>>>>>> parent of e77d43c (qwe):scenes/health_bar.gd

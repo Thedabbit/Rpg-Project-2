1ee6090e6e14334e643fefdb0d78161dadc6890a
PutthegundownG <167708770+PutthegundownG@users.noreply.github.com>
2024-05-15 20:53:30 -0700

    qwe

--- health_bar.gd
+++ health_bar.gd
@@ -8,12 +8,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-	pass # Replace with function body.
-========
 	if Global.enemy_amount == 1:
 		$".".queue_free()
 	elif Global.enemy_amount == 0:
-		$"../door2".queue_free()
->>>>>>>> f86afc107d3de67f2e2097848a711078b3082a9c:scenes/door.gd

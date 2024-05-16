a3a0e67a0db3550f24a50a64fda46fbb58480c9f
PutthegundownG <167708770+PutthegundownG@users.noreply.github.com>
2024-05-15 20:48:30 -0700

    Update door.gd

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -8,15 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-<<<<<<<< HEAD:health_bar.gd
-	pass
-
-
-func _on_texture_progress_bar_value_changed(value):
-	pass # Replace with function body.
-========
 	if Global.enemy_amount == 1:
 		$".".queue_free()
 	elif Global.enemy_amount == 0:
 		$"../door2".queue_free()
->>>>>>>> f86afc107d3de67f2e2097848a711078b3082a9c:scenes/door.gd
